f850ee8add01e0fc2226af7c53978e91b1780a7c
Nikolas Silva <nikolas.rsilva@gmail.com>
2019-04-10 01:50:30 -0300

    Add function to scrap all events from a page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const url = require('url');
+const querystring = require('querystring');
 
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
@@ -11,7 +12,7 @@ const FACEBOOK_EVENTS_PATHNAME = '/events';
  * TODO: write docs
  */
 const fetchPageContents = async (fbPageUrl) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
   await page.goto(fbPageUrl);
@@ -25,7 +26,7 @@ const fetchPageContents = async (fbPageUrl) => {
 /**
  * TODO: write docs
  */
-const fetchUpcomingEvents = async (fbPageUrl) => {
+const scrapUpcomingEvents = async (fbPageUrl) => {
   // TODO: Better validation and formatting of urls. It'd break if the `fbPageUrl` had a slash in the final
   const fbEventsPageUrl = `${fbPageUrl}${FACEBOOK_EVENTS_PATHNAME}`;
   const contents = await fetchPageContents(fbEventsPageUrl);
@@ -52,7 +53,7 @@ const fetchUpcomingEvents = async (fbPageUrl) => {
 /**
  * TODO: write docs
  */
-const scrapEventData = async (fbEventUrl) => {
+const scrapEvent = async (fbEventUrl) => {
   const contents = await fetchPageContents(fbEventUrl);
   const $ = cheerio.load(contents);
 
@@ -65,12 +66,59 @@ const scrapEventData = async (fbEventUrl) => {
     dateTime: {
       selector: '#event_time_info div[content]',
       attr: 'content',
-      convert: (dateStr) => new Date(dateStr),
+      convert: (dateStr) => {
+        const [start, end] = dateStr.split(' to ');
+        return {
+          start: new Date(start),
+          end: (end ? new Date(end) : undefined),
+        };
+      },
+    },
+    venue: {
+      selector: '#event_summary ul > li:nth-child(2) table tr > td:nth-child(2) > div > div:nth-child(1)',
+      data: {
+        name: 'a',
+        address: 'a + div',
+        pageUrl: {
+          selector: 'a',
+          attr: 'href',
+        },
+      },
+    },
+    ticketsUrl: {
+      selector: 'li[data-testid=event_ticket_link] > a',
+      attr: 'href',
+      // TODO: Transform this in a separate function
+      convert: (fbSafeUrl) => {
+        const parsedUrl = url.parse(fbSafeUrl);
+        const parsedQuery = querystring.parse(parsedUrl.query);
+
+        return parsedQuery.u;
+      },
+    },
+    details: {
+      selector: 'div[data-testid=event-permalink-details] span',
+      how: 'html',
     },
   });
 
-  console.log(data);
+  data.url = fbEventUrl;
   return data;
 };
 
-scrapEventData('https://facebook.com/events/1241375742677943/');
+/**
+ * TODO: write docs
+ */
+const scrapEventsFromPage = async (fbPageUrl) => {
+  const events = await scrapUpcomingEvents(fbPageUrl);
+  const promises = events.map(x => scrapEvent(x.url));
+
+  return await Promise.all(promises);
+};
+
+/** @module fb-events-scraper */
+module.exports = {
+  scrapEvent,
+  scrapEventsFromPage,
+  scrapUpcomingEvents,
+};
